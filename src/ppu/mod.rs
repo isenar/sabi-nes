31d6b38a197ac8c5c7f4b16c897274fad7f114d6
Dariusz Dajka <dariusz.dajka@gmail.com>
2021-11-20 11:48:40 +0100

    Move PPU registers to a separate struct

--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -1,11 +1,9 @@
 mod registers;
 
 use crate::cartridge::MirroringType;
-use crate::ppu::registers::{MaskRegister, ScrollRegister, StatusRegister};
+use crate::ppu::registers::PpuRegisters;
 use crate::{Address, Byte, Result};
 use anyhow::bail;
-use registers::AddressRegister;
-use registers::ControlRegister;
 
 #[derive(Debug)]
 pub struct Ppu {
@@ -15,18 +13,12 @@ pub struct Ppu {
     pub palette_table: [Byte; 32],
     /// 2KiB of space to hold background information
     pub vram: [Byte; 2048],
-    /// Internal memory to keep state of sprites (Object Attribute Memory)
-    pub oam_data: [Byte; 256],
+
     /// Mirroring type
     pub mirroring: MirroringType,
 
-    pub address_register: AddressRegister,
-    pub control_register: ControlRegister,
-    pub mask_register: MaskRegister,
-    pub status_register: StatusRegister,
-    pub scroll_register: ScrollRegister,
-
-    pub oam_address: Byte,
+    /// PPU registers
+    pub registers: PpuRegisters,
 
     internal_data_buffer: Byte,
 }
@@ -37,58 +29,53 @@ impl Ppu {
             chr_rom: chr_rom.into(),
             palette_table: [0; 32],
             vram: [0; 2048],
-            oam_data: [0; 256],
             mirroring,
-            address_register: Default::default(),
-            control_register: Default::default(),
+            registers: Default::default(),
             internal_data_buffer: Default::default(),
-            mask_register: Default::default(),
-            status_register: Default::default(),
-            scroll_register: Default::default(),
-            oam_address: 0,
         }
     }
 
     pub fn increment_vram_address(&mut self) {
-        self.address_register
-            .increment(self.control_register.vram_addr_increment());
+        self.registers
+            .address
+            .increment(self.registers.control.vram_addr_increment());
     }
 
     pub fn read_status_register(&self) -> Byte {
-        self.status_register.bits()
+        self.registers.status.bits()
     }
 
     pub fn read_oam_data(&self) -> Byte {
-        self.oam_data[self.oam_address as usize]
+        self.registers.oam_data[self.registers.oam_address as usize]
     }
 
     pub fn write_to_addr_register(&mut self, value: Byte) {
-        self.address_register.update(value);
+        self.registers.address.update(value);
     }
 
     pub fn write_to_control_register(&mut self, value: Byte) {
-        self.control_register.update(value);
+        self.registers.control.update(value);
     }
 
     pub fn write_to_mask_register(&mut self, value: Byte) {
-        self.mask_register.update(value);
+        self.registers.mask.update(value);
     }
 
     pub fn write_to_oam_address_register(&mut self, value: Byte) {
-        self.oam_address = value;
+        self.registers.oam_address = value;
     }
 
     pub fn write_to_oam_data(&mut self, value: Byte) {
-        self.oam_data[self.oam_address as usize] = value;
-        self.oam_address = self.oam_address.wrapping_add(1);
+        self.registers.oam_data[self.registers.oam_address as usize] = value;
+        self.registers.oam_address = self.registers.oam_address.wrapping_add(1);
     }
 
     pub fn write_to_scroll_register(&mut self, value: Byte) {
-        self.scroll_register.write(value);
+        self.registers.scroll.write(value);
     }
 
     pub fn write(&mut self, value: Byte) -> Result<()> {
-        let addr = self.address_register.get();
+        let addr = self.registers.address.get();
         self.increment_vram_address();
 
         match addr {
@@ -112,7 +99,7 @@ impl Ppu {
     }
 
     pub fn read(&mut self) -> Result<Byte> {
-        let addr = self.address_register.get();
+        let addr = self.registers.address.get();
         self.increment_vram_address();
 
         match addr {
--- src/ppu/registers/mod.rs
+++ src/ppu/registers/mod.rs
@@ -4,8 +4,37 @@ mod mask;
 mod scroll;
 mod status;
 
+use crate::Byte;
 pub use address::AddressRegister;
 pub use control::ControlRegister;
 pub use mask::MaskRegister;
 pub use scroll::ScrollRegister;
 pub use status::StatusRegister;
+
+const OAM_DATA_SIZE: usize = 256;
+
+#[derive(Debug)]
+pub struct PpuRegisters {
+    pub address: AddressRegister,
+    pub control: ControlRegister,
+    pub mask: MaskRegister,
+    pub scroll: ScrollRegister,
+    pub status: StatusRegister,
+    pub oam_address: Byte,
+    /// Internal memory to keep state of sprites (Object Attribute Memory)
+    pub oam_data: [Byte; OAM_DATA_SIZE],
+}
+
+impl Default for PpuRegisters {
+    fn default() -> Self {
+        Self {
+            address: Default::default(),
+            control: Default::default(),
+            mask: Default::default(),
+            scroll: Default::default(),
+            status: Default::default(),
+            oam_address: Default::default(),
+            oam_data: [0; OAM_DATA_SIZE],
+        }
+    }
+}
