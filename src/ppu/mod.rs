fdfb4fef5b2b96db136857c30decb7d987e81795
Dariusz Dajka <dariusz.dajka@gmail.com>
2021-11-21 10:31:26 +0100

    Add pallete table mirrors

--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -86,6 +86,12 @@ impl Ppu {
             }
             0x3000..=0x3eff => bail!("Requested invalid address from PPU ({:#x})", addr),
             0x3f00..=0x3fff => {
+                let mut addr = addr;
+                // "Addresses $3F10/$3F14/$3F18/$3F1C are mirrors of $3F00/$3F04/$3F08/$3F0C"
+                if [0x3f10, 0x3f14, 0x3f18, 0x3f1c].contains(&addr) {
+                    addr -= 0x10;
+                }
+
                 let offset_addr = addr - 0x3f00;
                 self.palette_table[offset_addr as usize] = value;
             }
@@ -118,6 +124,12 @@ impl Ppu {
             }
             0x3000..=0x3eff => bail!("Requested invalid address from PPU ({:#x})", addr),
             0x3f00..=0x3fff => {
+                let mut addr = addr;
+                // "Addresses $3F10/$3F14/$3F18/$3F1C are mirrors of $3F00/$3F04/$3F08/$3F0C"
+                if [0x3f10, 0x3f14, 0x3f18, 0x3f1c].contains(&addr) {
+                    addr -= 0x10;
+                }
+
                 let offset_addr = addr - 0x3f00;
                 Ok(self.palette_table[offset_addr as usize])
             }
