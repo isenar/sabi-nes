dd03246e65f94956eb5dcd0ff4a50e9c91c84559
Dariusz Dajka <dariusz.dajka@gmail.com>
2021-11-22 12:35:35 +0100

    Remove unnecessary itype field from Interrupt

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,20 +1,13 @@
 use crate::{Address, Byte};
 
-#[derive(Debug, Clone, Copy)]
-pub enum InterruptType {
-    Nmi,
-}
-
 #[derive(Debug)]
 pub struct Interrupt {
-    pub itype: InterruptType,
     pub vector_addr: Address,
     pub break_flag_mask: Byte,
     pub cpu_cycles: u8,
 }
 
 pub const NMI: Interrupt = Interrupt {
-    itype: InterruptType::Nmi,
     vector_addr: 0xfffa,
     break_flag_mask: 0b0010_0000,
     cpu_cycles: 2,
