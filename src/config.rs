0abdb59fd26da397a6e13db2110de4073874b43d
Darek Dajka <darek.dajka@hyperexponential.com>
2022-10-31 00:44:37 +0100

    Update clap to v4

--- src/config.rs
+++ src/config.rs
@@ -1,14 +1,15 @@
+use clap::Parser;
 use std::path::PathBuf;
 
-#[derive(Debug, clap::Parser)]
+#[derive(Debug, Parser)]
 pub struct Config {
-    #[clap(parse(from_os_str), long = "rom-path")]
+    #[arg(long = "rom-path")]
     pub rom_path: PathBuf,
-    #[clap(default_value = "256", long = "width")]
+    #[arg(default_value = "256", long = "width")]
     pub window_width: u32,
-    #[clap(default_value = "240", long = "height")]
+    #[arg(default_value = "240", long = "height")]
     pub window_height: u32,
-    #[clap(default_value = "3", long = "scale")]
+    #[arg(default_value = "3", long = "scale")]
     pub scale: u32,
 }
 
